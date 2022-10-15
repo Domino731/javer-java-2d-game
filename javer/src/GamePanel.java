82ba8403f2e593984d65abf395779ca1945159a3
Domino731 <d.orzechowski18@gmail.com>
2022-10-15 16:45:41 +0200

    FPS counter

--- javer/src/GamePanel.java
+++ javer/src/GamePanel.java
@@ -9,6 +9,9 @@ import java.awt.*;
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private int xDelta = 100, yDelta = 100;
+    // fps counter
+    private int  frames = 0;
+    private long lastCheck = 0;
 
     public GamePanel(){
         mouseInputs = new MouseInputs(this);
@@ -20,22 +23,31 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         xDelta += value;
-        repaint();
     }
 
     public void changeYDelta(int value){
         yDelta += value;
-        repaint();
     }
 
     public void setDelta(int x, int y){
         xDelta = x;
         yDelta = y;
-        repaint();
+    }
+
+    private void checkFps(){
+        frames++;
+        if(System.currentTimeMillis() - lastCheck >= 1000){
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.drawRect( xDelta,  yDelta, 100, 100);
+
+        checkFps();
+        repaint();
     }
 }
