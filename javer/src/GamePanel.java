05bedffa58d9b83bd9bb705044ed0e9790d56a47
Domino731 <d.orzechowski18@gmail.com>
2022-10-15 16:22:59 +0200

    Listener for mouse

--- javer/src/GamePanel.java
+++ javer/src/GamePanel.java
@@ -1,13 +1,20 @@
 package javer.src;
 
 import javer.src.inputs.KeyboardInputs;
+import javer.src.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+
     public GamePanel(){
+        mouseInputs = new MouseInputs();
+
         addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
         System.out.println("GamePanel created");
     }
 
